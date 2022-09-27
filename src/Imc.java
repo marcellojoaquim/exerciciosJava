8b5c50116d8b371199642a6108f1cdfeed2eca56
Marcello Joaquim da Silva <marcellojoaquim1@hotmail.com>
2022-09-27 15:05:47 -0300

    concluindo cálculo imc

--- src/Imc.java
+++ src/Imc.java
@@ -4,7 +4,24 @@ import java.util.Scanner;
 public class Imc {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);    
-        
+        float peso=0, altura=0, imc=0;
+        System.out.println("informe os valores de peso e altura");
+        peso = input.nextFloat();
+        altura = input.nextFloat();
+        input.close();
+        imc = (peso/(altura*altura));
+
+        if(imc<20){
+            System.out.println("IMC = "+imc+" - Abaixo do peso");
+        }else if((imc>=20)&&(imc<=25)){
+            System.out.println("IMC = "+imc+" - Peso Normal");
+        }else if((imc>25)&&(imc<=30)){
+            System.out.println("IMC = "+imc+" - Sobrepeso");
+        }else if((imc>30)&&(imc<=40)){
+            System.out.println("IMC = "+imc+" - Obeso");
+        }else if(imc>40){
+            System.out.println("IMC = "+imc+" - Obeso Mórbido");
+        }
         
     }
 }
